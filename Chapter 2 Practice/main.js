8029a16fdaeb5f22c4e6040a3c18e13d3098146e
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2020-09-04 17:37:32 +0500

    Chapter 2 Practice

---
+++ Chapter 2 Practice/main.js
@@ -0,0 +1,22 @@
+//var username;
+//var myname = "Bazil Farooq";
+//var message ="Hello World"
+//alert(message);
+
+//var name = "Bazil Farooq"
+//var age = "I am 20 years old."
+//var bio = "Student in University."
+//alert(name)
+//alert(age)
+//alert(bio)
+
+//var food = "Pizza"
+//alert(food+"\n"+food[0]+food[1]+food[2]+food[3]+"\n"+food[0]+food[1]+food[2]+"\n"+food[0]+food[1]+"\n"+food[0])
+
+//var email = "mianbazil21@gmail.com"
+//alert("My email is "+email)
+ //var book = "A smarter way to learn JavaScript"
+ //alert("I am trying to learn from the book "+book)
+
+ //var str = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+ //alert(str)
