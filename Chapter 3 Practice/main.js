6dc1b6af92425f8b7ee500a99a5702a0012e6536
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2020-09-04 17:58:54 +0500

    Chapter 3 Practice

---
+++ Chapter 3 Practice/main.js
@@ -0,0 +1,11 @@
+//var age = 20
+//alert("I am "+age+" years old.")
+//var sum = 0
+ //sum+=1 
+ //alert("You can visited this web page "+ sum +"times")
+ //var birthyear = 2000
+ //document.write("My birth year is "+birthyear+"<br>"+"Data Type of my birth year is number.")
+ //var name = prompt("Enter Your Name.")
+ //var producttitle = prompt("Enter Your Product Name.")
+ //var quantity = prompt("Enter Product Quantity.")
+ //document.write(name+ " ordered "+quantity+ producttitle+" on XYZ Clothing store.")
