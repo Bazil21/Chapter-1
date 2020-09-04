64eb293cb1782520d7aaab5eca9e5f32fe0282a3
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2020-09-04 17:02:18 +0500

    Chapter 1 Practice

---
+++ main.js
@@ -0,0 +1,7 @@
+//var name=prompt("Enter Your Name.")
+//alert( " Welcome!  "+name +" Nice to meet you....")
+
+//alert("Error! Please Enter Valid Password.")
+//alert("Welcome to JS Land.\nHappy Coding!")
+//alert("Welcome to JS Land!")
+//alert("Happy Coding.")
