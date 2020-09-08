ff943af86bd54f85706d03941bfee0572c7663fd
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2020-09-08 17:39:52 +0500

    Chapter 4 Practice

---
+++ Chapter 4 Practice/main.js
@@ -0,0 +1,22 @@
+//I Declare 3 Variables in One Statement
+var x,z,y
+
+//I Declare 5 legall Variables
+var $a
+var _shop
+var Car
+var car1
+var Car_2
+
+//I declare 5 ilegall variables
+var 1a
+var @g
+var red car
+var .abc
+var 32b
+
+document.write("<h1>Rules for Naming JS Variables</h1> <br>")
+document.write("Variables name can only contains Number ,$ and _. For Example $my_1st Variable<br>")
+document.write("Number must begin with a letter $,_.For Example $name , _name or name<br>")
+document.write("Variables names are case sensitive<br>")
+document.write("Variables name should not be JS keywords<br>")
