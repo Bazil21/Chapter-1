75e091bdc99b9450f85d5515a2bdd7ae960f5098
Bazil21 <59576110+Bazil21@users.noreply.github.com>
2020-09-10 17:50:27 +0500

    update

---
+++ Exercise 6 to 9/main.js
@@ -0,0 +1,47 @@
+//Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+var a = 5;
+var b = 10;
+var c = a + b;
+document.write("Sum of " + a +" and  "+ b +" "+ " is " +c +"<br>"+"<br>");
+
+var a = 20;
+var b = 10;
+var c = a - b;
+document.write("Subtraction of " + a +" and  "+ b +" "+ " is " +c +"<br>" +"<br>");
+
+var a = 50;
+var b = 10;
+var c = a / b;
+document.write("Division of " + a +" and  "+ b +" "+ " is " +c +"<br>" +"<br>");
+var a = 9;
+var b = 4;
+var c = a % b;
+document.write("Modulus of " + a +" and  "+ b +" "+ " is " +c +"<br>" +"<br>");
+
+
+//Declare a variable.  b. Show the value of variable in your browser like “Value  after variable declaration is: ??”.
+var x;
+document.write("Value after variable declaration is "+x +"<br>" +"<br>");
+
+//c. Initialize the variable with some number. d. Show the value of variable in your browser like “Initial value: 5”.
+ x = 5;
+document.write("Initial Value  " + x  +"<br>" +"<br>")
+
+//Increment the variable. f. Show the value of variable in your browser like “Value after increment is: 6”
+ x++;
+ document.write("Value After Increment :" + x +"<br>" +"<br>")
+
+//Add 7 to the variable.  h. Show the value of variable in your browser like “Value MATH EXPRESSIONS 
+ //after addition is: 13”.
+ x = x + 7
+ document.write("Value After Addition : " +x +"<br>" +"<br>")
+
+ //Show the value of variable in your browser like “Value after decrement is: 12”.
+ x--;
+ document.write("Value After Decrement : "+ x +"<br>" +"<br>")
+ 
+ // Show the remainder after dividing the variable’s value by 3.
+ //Output : “The remainder is : 0”.
+ x  = x / 3
+ x = x % x
+ document.write("Remainder after dividing the variables value by 3 : " + x)
